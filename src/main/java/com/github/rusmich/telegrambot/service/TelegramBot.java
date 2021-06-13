6547d55291aa5b807e7b21238ab7f9bc9e76e8a5
Vladislav <matayes111@gmail.com>
2021-06-13 19:36:25 +0300

    Добавил склонение минут

--- src/main/java/com/github/rusmich/telegrambot/service/TelegramBot.java
+++ src/main/java/com/github/rusmich/telegrambot/service/TelegramBot.java
@@ -70,7 +70,7 @@ public class TelegramBot extends TelegramLongPollingBot {
                     if (words.length == 2 || words.length == 3 && words[2].equalsIgnoreCase("сек") ||
                             words[2].equalsIgnoreCase("секунд") || words[2].equalsIgnoreCase("секунды")) {
                         long time = userTimer * 1000L;
-                        //Склонение секунд.
+                        //Склонение секунд при двух элементах массива
                         if (words.length == 2) {
                             if(Integer.parseInt(new String(words[1].toCharArray())) == 1){
                                 sendMessage("Таймер установлен на " + userTimer + " секунду", chatId);
@@ -108,7 +108,7 @@ public class TelegramBot extends TelegramLongPollingBot {
                                 }
                                 sendMessage("@" + userName + " Таймер на " + userTimer + " секунд окончен", chatId);
                             }
-                            //Склонение секунд.
+                            //При трёх элементах массива
                         } else if (words.length == 3) {
                             if(Integer.parseInt(new String(words[1].toCharArray())) == 1){
                                 sendMessage("Таймер установлен на " + userTimer + " секунду", chatId);
@@ -145,6 +145,85 @@ public class TelegramBot extends TelegramLongPollingBot {
                                     e.printStackTrace();
                                 }
                                 sendMessage("@" + userName + " Таймер на " + userTimer + " секунд окончен", chatId);
+                            }
+                        }
+                    } else if (words.length == 3 && words[2].equals("мин") || words[2].equals("минут") || words[2].equals("минуты")) {
+                        long time = userTimer * 600_00L;
+
+                        if (words.length == 2) {
+                            if(Integer.parseInt(new String(words[1].toCharArray())) == 1){
+                                sendMessage("Таймер установлен на " + userTimer + " минуту", chatId);
+                                try {
+                                    Thread.sleep(time);   //тестовое время 20 секунд, проблема способа, в том что нету мультипоточности
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                sendMessage("@" + userName + " Таймер на " + userTimer + " минуту окончен", chatId);
+                            } else if (Integer.parseInt(new String(words[1].toCharArray())) > 11 &&
+                                    Integer.parseInt(new String(words[1].toCharArray())) < 15) {
+                                sendMessage("Таймер установлен на " + userTimer + " минут", chatId);
+                                try {
+                                    Thread.sleep(time);   //тестовое время 20 секунд, проблема способа, в том что нету мультипоточности
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                sendMessage("@" + userName + " Таймер на " + userTimer + " минут окончен", chatId);
+                            } else if ((Integer.parseInt(new String(words[1].toCharArray())) % 10) == 2 ||
+                                    (Integer.parseInt(new String(words[1].toCharArray())) % 10) == 3 ||
+                                    (Integer.parseInt(new String(words[1].toCharArray())) % 10) == 4) {
+                                sendMessage("Таймер установлен на " + userTimer + " минуты", chatId);
+                                try {
+                                    Thread.sleep(time);   //тестовое время 20 секунд, проблема способа, в том что нету мультипоточности
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                sendMessage("@" + userName + " Таймер на " + userTimer + " минуты окончен", chatId);
+                            } else {
+                                sendMessage("Таймер установлен на " + userTimer + " минут", chatId);
+                                try {
+                                    Thread.sleep(time);   //тестовое время 20 секунд, проблема способа, в том что нету мультипоточности
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                sendMessage("@" + userName + " Таймер на " + userTimer + " минут окончен", chatId);
+                            }
+                            //Склонение секунд.
+                        } else if (words.length == 3) {
+                            if(Integer.parseInt(new String(words[1].toCharArray())) == 1){
+                                sendMessage("Таймер установлен на " + userTimer + " минуту", chatId);
+                                try {
+                                    Thread.sleep(time);   //тестовое время 20 секунд, проблема способа, в том что нету мультипоточности
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                sendMessage("@" + userName + " Таймер на " + userTimer + " минуту окончен", chatId);
+                            } else if (Integer.parseInt(new String(words[1].toCharArray())) > 11 &&
+                                    Integer.parseInt(new String(words[1].toCharArray())) < 15) {
+                                sendMessage("Таймер установлен на " + userTimer + " минут", chatId);
+                                try {
+                                    Thread.sleep(time);   //тестовое время 20 секунд, проблема способа, в том что нету мультипоточности
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                sendMessage("@" + userName + " Таймер на " + userTimer + " минут окончен", chatId);
+                            } else if ((Integer.parseInt(new String(words[1].toCharArray())) % 10) == 2 ||
+                                    (Integer.parseInt(new String(words[1].toCharArray())) % 10) == 3 ||
+                                    (Integer.parseInt(new String(words[1].toCharArray())) % 10) == 4) {
+                                sendMessage("Таймер установлен на " + userTimer + " минуты", chatId);
+                                try {
+                                    Thread.sleep(time);   //тестовое время 20 секунд, проблема способа, в том что нету мультипоточности
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                sendMessage("@" + userName + " Таймер на " + userTimer + " минуты окончен", chatId);
+                            } else {
+                                sendMessage("Таймер установлен на " + userTimer + " минут", chatId);
+                                try {
+                                    Thread.sleep(time);   //тестовое время 20 секунд, проблема способа, в том что нету мультипоточности
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                sendMessage("@" + userName + " Таймер на " + userTimer + " минут окончен", chatId);
                             }
                         }
                     }
